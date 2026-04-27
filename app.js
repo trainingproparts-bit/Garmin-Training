74ef92bb73bbea69c1551f0e5ebf8704fa069f05
trainingproparts-bit <training.proparts@gmail.com>
2026-04-27 11:48:04 -0300

    Rename function enviarResultado to registerResult

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const SHEETS_URL = "https://script.google.com/macros/s/AKfycbyMWnxsNOLK9yR9D8KfZfxilFMzN267r56Dhq4CTHwMCu75uNd1v8Z-s6TE_mC66N9x/exec";
 
-function enviarResultado(nome, modulo, nota, acertos, total) {
+function registerResult(nome, modulo, nota, acertos, total) {
   fetch(SHEETS_URL, {
     method: "POST",
     body: JSON.stringify({
